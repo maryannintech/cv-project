e9018c352d7a26464e814fcd726e3649a3830ee4
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-07-06 14:23:15 +0800

    tried the onClick in title button

---
+++ src/components/PreviewCv.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Title.css";
+import "../styles/Fonts.css";
+
+function PreviewCv() {
+  return (
+    <div className="preview-cv">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default PreviewCv;
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import "../styles/Title.css";
+import PreviewCv from "./PreviewCv";
 import "../styles/Fonts.css";
 
 function Title(props) {
   const { title, tagline } = props;
+  const [isShown, setIsShown] = useState(false);
+  function showPreviewCV() {
+    setIsShown((current) => !current);
+  }
   return (
     <div className="app-name">
       <div className="container">
         <h1>{title}</h1>
         <h3>{tagline}</h3>
       </div>
-      <div></div>
+      <div className="title-buttons">
+        <button className="preview-cs" onClick={showPreviewCV}>
+          PREVIEW
+        </button>
+        {isShown && <PreviewCv></PreviewCv>}
+      </div>
     </div>
   );
 }
