2bdb7ecec8b175c676547a697ca039c2fafc9711
mary ann <114210058+maryannintech@users.noreply.github.com>
2023-07-10 14:51:14 +0800

    changes

--- src/components/EducationalBg.js
+++ src/components/EducationalBg.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "../styles/Fonts.css";
 import "../styles/EducationalBg.css";
 
 function EducationalBg({ educFormValues, onChange }) {
   const [educForms, setEducForms] = useState([]);
+  const inputRefs = useRef([]);
+
+  useEffect(() => {
+    // Create refs for input fields dynamically based on the educForms length
+    inputRefs.current = Array(educForms.length)
+      .fill()
+      .map((_, i) => inputRefs.current[i] || React.createRef());
+  }, [educForms.length]);
 
   const addEducForm = () => {
     const updatedForms = [
@@ -16,13 +24,13 @@ function EducationalBg({ educFormValues, onChange }) {
       }
     ];
     setEducForms(updatedForms);
-    onChange(updatedForms); // Pass the updated forms back to the App component
+    onChange(updatedForms);
   };
 
   const removeEducForm = () => {
     const updatedForms = educForms.slice(0, -1);
     setEducForms(updatedForms);
-    onChange(updatedForms); // Pass the updated forms back to the App component
+    onChange(updatedForms); 
   };
 
   const handleFormChange = (index, e) => {
@@ -37,57 +45,55 @@ function EducationalBg({ educFormValues, onChange }) {
       return form;
     });
     setEducForms(updatedForms);
-    onChange(updatedForms); // Pass the updated forms back to the App component
+    onChange(updatedForms); 
   };
-
-  function EducForm({ index }) {
-    return (
-      <form>
-        <label htmlFor={`school${index}`}>School attended: </label>
-        <input
-          type="text"
-          id={`school${index}`}
-          name="school"
-          required
-          value={educForms[index].school}
-          onChange={(e) => handleFormChange(index, e)}
-        ></input>
-        <label htmlFor={`program${index}`}>Program: </label>
-        <input
-          type="text"
-          id={`program${index}`}
-          name="program"
-          required
-          value={educForms[index].program}
-          onChange={(e) => handleFormChange(index, e)}
-        ></input>
-        <label htmlFor={`started${index}`}>Date started: </label>
-        <input
-          type="date"
-          id={`started${index}`}
-          name="started"
-          required
-          value={educForms[index].started}
-          onChange={(e) => handleFormChange(index, e)}
-        ></input>
-        <label htmlFor={`graduated${index}`}>Date graduated: </label>
-        <input
-          type="date"
-          id={`graduated${index}`}
-          name="graduated"
-          required
-          value={educForms[index].graduated}
-          onChange={(e) => handleFormChange(index, e)}
-        ></input>
-      </form>
-    );
-  }
 
   return (
     <div className="education-bg">
       <h1>EDUCATIONAL BACKGROUND</h1>
       {educForms.map((_, index) => (
-        <EducForm key={index} index={index} />
+        <form key={index}>
+          <label htmlFor={`school${index}`}>School attended: </label>
+          <input
+            ref={(el) => (inputRefs.current[index] = el)}
+            type="text"
+            id={`school${index}`}
+            name="school"
+            required
+            value={educForms[index].school}
+            onChange={(e) => handleFormChange(index, e)}
+          ></input>
+          <label htmlFor={`program${index}`}>Program: </label>
+          <input
+            ref={(el) => (inputRefs.current[index] = el)}
+            type="text"
+            id={`program${index}`}
+            name="program"
+            required
+            value={educForms[index].program}
+            onChange={(e) => handleFormChange(index, e)}
+          ></input>
+          <label htmlFor={`started${index}`}>Date started: </label>
+          <input
+            ref={(el) => (inputRefs.current[index] = el)}
+            type="date"
+            id={`started${index}`}
+            name="started"
+            required
+            value={educForms[index].started}
+            onChange={(e) => handleFormChange(index, e)}
+          ></input>
+          <label htmlFor={`graduated${index}`}>Date graduated: </label>
+          <input
+            ref={(el) => (inputRefs.current[index] = el)}
+            type="date"
+            id={`graduated${index}`}
+            name="graduated"
+            required
+            value={educForms[index].graduated}
+            onChange={(e) => handleFormChange(index, e)}
+          ></input>
+        </form>
       ))}
       <div className="educ-btns">
         <button className="add-educ" onClick={addEducForm}>
